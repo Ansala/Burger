071b73d20b1ff63acae724b3ac2805568e981b87
Ansala <ansala1821@hotmail.com>
2019-04-13 19:56:32 -0400

    Changes

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Ham Burger', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie', FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chicken Burger', FALSE);
